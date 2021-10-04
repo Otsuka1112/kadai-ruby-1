39c91f7f6343e4c6ee57a746c0706905bdff014a
taishin.otsuka <otktshn@gmail.com>
2021-10-04 11:52:56 +0000

    課題再提出用

--- kadai-introduce.rb
+++ kadai-introduce.rb
@@ -1,8 +1,4 @@
-str1 = "大塚" 
-str2 = "太慎" 
-honmyo = str1 + str2 
-str3 = "です。"
-num = "27"
-str4 = "歳です。"
-kadai = honmyo + str3 + num + str4
-puts kadai
+name = "大塚太慎"
+age= 27
+introduce = name + "です。" + age.to_s+ "歳です"
+puts introduce
